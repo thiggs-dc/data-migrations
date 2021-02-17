45e3a52064abfc949ec4246a8b252e06f33a5e4d
Ryan Langton <Ryan.Langton@questanalytics.com>
2021-02-17 07:34:37 -0600

    add to evolve code and scripts

---
+++ evolve/migrations_to_be_moved/V1_0_1__Alter_CustomerTable_AddAddressFields.sql
@@ -0,0 +1,6 @@
+ALTER TABLE [dbo].[Customer]
+	ADD [Address1] [nvarchar](200) NULL,
+		[Address2] [nvarchar](200) NULL,
+		[City] [nvarchar](100) NULL,
+		[State] [nvarchar](50) NULL,
+		[Zip] [nvarchar](20) NULL
