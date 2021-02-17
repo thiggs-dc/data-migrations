a1381530890962f3e79e77e0d432d9f70cbfaa03
Ryan Langton <Ryan.Langton@questanalytics.com>
2021-02-17 11:58:11 -0600

    create 2 versions of the view

---
+++ evolve/migrations_to_be_moved/R__Customer_view.sql
@@ -0,0 +1,6 @@
+CREATE OR ALTER VIEW [dbo].[vCustomer]
+AS
+  SELECT c.Name, c.Id,
+  (select COUNT(*) from [dbo].[${orderTable}] o where o.CustomerId = c.Id) AS OrderCount
+  FROM  dbo.Customer c
+GO
--- evolve/views/R__Customer_view.sql
+++ evolve/views/R__Customer_view.sql
@@ -1,6 +1,5 @@
 CREATE OR ALTER VIEW [dbo].[vCustomer]
 AS
   SELECT c.Name, c.Id
-  -- ,(select COUNT(*) from [dbo].[${orderTable}] o where o.CustomerId = c.Id) AS OrderCount
   FROM  dbo.Customer c
 GO
